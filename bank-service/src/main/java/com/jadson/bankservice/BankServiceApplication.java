27611e1a4463e404944fcf3c1ccd5cffbd4073bb
jadson76 <biojadson@gmail.com>
2022-07-19 21:15:47 -0300

    Adiciona configurações iniciais no properties. Adiciona arquivo script de banco.

--- bank-service/src/main/java/com/jadson/bankservice/BankServiceApplication.java
+++ bank-service/src/main/java/com/jadson/bankservice/BankServiceApplication.java
@@ -2,12 +2,13 @@ package com.jadson.bankservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan
 public class BankServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankServiceApplication.class, args);
 	}
-
 }
