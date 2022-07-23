1ab4678a99839a031381b0e304b224b54a832c4a
jadson76 <biojadson@gmail.com>
2022-07-23 19:39:45 -0300

    Commit inicial.

---
+++ buy-ticket-service/src/main/java/com/jadson/microservices/buyticket/BuyTicketApplication.java
@@ -0,0 +1,13 @@
+package com.jadson.microservices.buyticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyTicketApplication.class, args);
+	}
+
+}
---
+++ buy-ticket-service/src/test/java/com/jadson/microservices/buyticket/BuyTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadson.microservices.buyticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
