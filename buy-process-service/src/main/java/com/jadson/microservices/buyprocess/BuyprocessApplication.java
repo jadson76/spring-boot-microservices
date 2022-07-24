de83af61ccac1e90e327cdc25f3bd16d5b85cbd7
jadson76 <biojadson@gmail.com>
2022-07-24 17:28:05 -0300

    buy-process
    commit inicial buy-process

---
+++ buy-process-service/src/main/java/com/jadson/microservices/buyprocess/BuyprocessApplication.java
@@ -0,0 +1,13 @@
+package com.jadson.microservices.buyprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyprocessApplication.class, args);
+	}
+
+}
---
+++ buy-process-service/src/main/java/com/jadson/microservices/buyprocess/gateway/BankRetornoJson.java
@@ -0,0 +1,6 @@
+package com.jadson.microservices.buyprocess.gateway;
+
+
+public class BankRetornoJson {
+    private String mensagem;
+}
